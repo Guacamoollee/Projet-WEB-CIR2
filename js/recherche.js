067076824a471474b25f793b56c57006a7441eeb
hippo-Cir <66952675+hippo-Cir@users.noreply.github.com>
2021-06-23 15:27:36 +0200

    Update recherche.js

--- js/recherche.js
+++ js/recherche.js
@@ -8,32 +8,33 @@ function affiche_isen(poll){
     $('#depart').append('<option value="'+isen.site_isen+'"> '+isen.site_isen+' </option>' );
     }
 }
+//event 
 $('#boutton').click((event) =>
-  {
-    console.log(event);
-    event.preventDefault();
-    if($('#Partir').is(':checked'))
-        ajaxRequest('GET', '/php/api/v1/api.php/trajets?destination=%'+$('#addr').val()+'%&isen_depart=%'+$('#depart').val()+'%', info);//aller
-    else
-        ajaxRequest('GET', '/php/api/v1/api.php/trajets?depart=%'+$('#addr').val()+'%&isen_destinataire=%'+$('#depart').val()+'%', info);//partir
+{
+console.log(event);
+        event.preventDefault();
+        if($('#Partir').is(':checked'))
+            ajaxRequest('GET', '/php/api/v1/api.php/trajets?destination='+$('#adr').val()+'%&isen_depart=%'+$('#depart').val()+'%', info);//aller
+        else
+            ajaxRequest('GET', '/php/api/v1/api.php/trajets?depart='+$('#adr').val()+'%&isen_destinataire=%'+$('#depart').val()+'%', info);//partir
   }
-  
 );
 
+//affichage des données de la base de donnée  
 
 function info(vaar){
+    $('#resultat').html('')
     for(let vaars of vaar){
-        
-        $('#resultat').append('<p>test: '+vaars.pseudo+ '</p><br>')
-        $('#resultat').append(vaars.adresse+'<br>')
-        $('#resultat').append(vaars.commune+'<br>')
-        $('#resultat').append(vaars.code_postal+'<br>')
-        $('#resultat').append(vaars.site_isen+'<br>')
-        $('#resultat').append(vaars.date_heure_depart+'<br>')
-        $('#resultat').append(vaars.date_heure_arrivee+'<br>')
-        $('#resultat').append(vaars.nb_places_max+'<br>')
-        $('#resultat').append(vaars.prix+'<br>')
-        $('#resultat').append('<input type=submit value=reserver id='+vaars.id_trajet+'></input>')
+        $('#resultat').append('<p>Pseudo: '+vaars.pseudo+ '</p></div><br>')//affichage du pseudo 
+        $('#resultat').append('<p> Adresse: '+vaars.adresse+'</p><br>')//affichage de l'adresse
+        $('#resultat').append('<p> Commune: '+vaars.commune+'</p><br>')//affichage de la commune
+        $('#resultat').append('<p> Code Postal: '+vaars.code_postal+'</p><br>')//affichage du code postal
+        $('#resultat').append('<p> ISEN: '+vaars.site_isen+'</p><br>')//affichage du site ISEN
+        $('#resultat').append('<p> Date et heure de départ: '+vaars.date_heure_depart+'</p><br>')// affichage de l'heure de départ
+        $('#resultat').append('<p> Date et heure d arrivée: '+vaars.date_heure_arrivee+'</p><br>')// affichage de l'heure d'arrivée
+        $('#resultat').append('<p> Nombre de place:'+vaars.nb_places_max+'</p><br>')//affichage du nombre de place
+        $('#resultat').append('<p> Prix : '+vaars.prix+'€</p><br>')//affichage du prix
+        $('#resultat').append('<input type=submit value=reserver id='+vaars.id_trajet+'></input>')// boutton reserver 
     }   
-    console.log(vaar);
 }
+
