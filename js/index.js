77cc01f93caabec77b1902a7ce99a0e642fcfc7b
hippo-Cir <66952675+hippo-Cir@users.noreply.github.com>
2021-06-23 09:26:50 +0200

    Delete index.js

--- js/index.js
+++
@@ -1,7 +0,0 @@
-/*$("#Aller_Partir").change(function(){
-    var value=$(this).val();
-    console.log(value);
-    $("#formulaire").html("");
-    $("#formulaire").append('<select name="Site de depart" id="depart"> <option value="">veuillez choisir</option><option value="Brest">Brest</option><option value="Nantes">Nantes</option>    <option value="Caen">Caen</option><option value="Rennes">Rennes</option></select>')
-})*/
-
